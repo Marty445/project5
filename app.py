41a6ae12ed4f1456496d30cf3abd96401622ee99
Marty445 <markovskimarty@gmail.com>
2026-02-11 14:06:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
-st.write()
-st.text_input()
-st.number_input()
-if / else
+import streamlit as st
+st.write("What is the largest ocean in the world?")
+st.text_input("The Pacific Ocean")
+answer = st.radio(
+  "What is the largest ocean in the world?",
+  ("The Pacific Ocean", "ne")
+)
+if answer == "The Pacific Ocean":
+  st.write("Correct!")
+else:
+  st.write("Incorrect!)
+  
 st.button()
