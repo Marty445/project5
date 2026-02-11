0dd458ce5aa63a808b54e212a136a51a3504aad4
Marty445 <markovskimarty@gmail.com>
2026-02-11 14:25:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,28 +8,28 @@ if st.button("Check answer for question 1"):
   else:
     st.error("Incorrect!")
 st.write("Which country has the largest population in the world?")
-user_answer1 = st.text_input(" Your answer", key "w")
+user_answer1 = st.text_input(" Your answer", key="w")
 if st.button("Check answer for question 2"):
   if user_answer1.strip().lower() == "India":
     st.success("Correct!")
   else:
     st.error("Incorrect!")
 st.write("What is the longest river in the world?")
-user_answer1 = st.text_input(" Your answer", key "w")
+user_answer1 = st.text_input(" Your answer", key="w")
 if st.button("Check answer for question 3"):
   if user_answer1.strip().lower() == "The Nile River":
     st.success("Correct!")
   else:
     st.error("Incorrect!")
 st.write("Which continent is the Sahara Desert located on?")
-user_answer1 = st.text_input(" Your answer", key "w")
+user_answer1 = st.text_input(" Your answer", key="w")
 if st.button("Check answer for question 4"):
   if user_answer1.strip().lower() == "Africa":
     st.success("Correct!")
   else:
     st.error("Incorrect!")
 st.write("What is the capital city of Japan?")
-user_answer1 = st.text_input(" Your answer", key "w")
+user_answer1 = st.text_input(" Your answer", key="w")
 if st.button("Check answer for question 5"):
   if user_answer1.strip().lower() == "Tokyo":
     st.success("Correct!")
